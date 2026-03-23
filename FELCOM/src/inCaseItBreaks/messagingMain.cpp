5000a037e7e9e83e3b01fae9558968fc5da0d5ad
Marc Gedeon <marc.e.gedeon27@gmail.com>
2026-03-23 13:14:58 +0200

    fix: commented out inCaseItBreaks files

--- FELCOM/src/inCaseItBreaks/messagingMain.cpp
+++ FELCOM/src/inCaseItBreaks/messagingMain.cpp
@@ -2,6 +2,7 @@
 //this is the main.cpp that made texting work
 //==================================
 
+/*
 #include <config.h>
 #include <debug.h>
 
@@ -70,68 +71,4 @@ void loop() {
     renderChat(Log, Input);
     delay(30);
 }
-
-
-
-
-/*
-//PongGame game;
-//bool leftPaddleSelected = false;
-
-// transceiver radio;
-
-/*void rotaryEncoderCW()
-{
-  if (leftPaddleSelected)
-  {
-    moveLeftPaddle(&game, 1);
-  }
-  else
-  {
-    moveRightPaddle(&game, 1);
-  }
-}
-
-void rotaryEncoderCCW()
-{
-  if (leftPaddleSelected)
-  {
-    moveLeftPaddle(&game, -1);
-  }
-  else
-  {
-    moveRightPaddle(&game, -1);
-  }
-}
-
-void rotaryEncoderButtonPressed()
-{
-  leftPaddleSelected = !leftPaddleSelected;
-}
 */
-/*void setup(){
-  /*#if DEBUG
-    loggerSetup();
-  #endif
-    initializeGame(&game);
-    setupDisplay();
-
-  // input choice
-  #if defined(USE_ROTARY_ENCODER) && USE_ROTARY_ENCODER
-    setupRotaryEncoder();
-  #else
-    joystickInit();
-  #endif
-
-    // radio.setup();
-    // radio.setMode(RECEIVE);
-    // pinMode(2, OUTPUT);
-  }*/
-
-/*void loop(){
-  // delay(500);
-  // digitalWrite(2, HIGH);
-  // delay(500);
-  // digitalWrite(2, LOW);
-  //drawGame(&game);
-}*/
