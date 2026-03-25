79a8ad80bcc6832b47bdd5cb5a1de3e5ff61df13
Charbel Assaad <sydiepus@gmail.com>
2026-03-25 20:02:32 +0200

    fix: remove unnecessary include from config.h

--- FELCOM/lib/debug/debug.cpp
+++ FELCOM/lib/debug/debug.cpp
@@ -1,5 +1,6 @@
 #include "debug.h"
 
+#include <Arduino.h>
 #include <config.h>
 #include <stdio.h>
 
--- FELCOM/lib/encyption/encryption.h
+++ FELCOM/lib/encyption/encryption.h
@@ -2,6 +2,7 @@
 #define ENCRYPTION_H
 
 #include <config.h>
+#include <stddef.h>
 
 void encrypt(char* text, size_t len);
 void decrypt(char* text, size_t len);
